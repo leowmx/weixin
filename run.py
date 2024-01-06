87e7aa49fc504d5edaa5d6400ba61b80791e0807
王铭瑄 <leowmx@163.com>
2024-01-06 12:43:07 +0800

    updata

--- run.py
+++ run.py
@@ -1,8 +1,19 @@
-# 创建应用实例
+#  创建应用实例
+# import sys
+
+# from wxcloudrun import app
+
+#  启动Flask Web服务
+# if __name__ == '__main__':
+#     app.run(host=sys.argv[1], port=sys.argv[2])
+
 import sys
+from flask import *
 
-from wxcloudrun import app
+app = Flask(__name__)
 
-# 启动Flask Web服务
-if __name__ == '__main__':
-    app.run(host=sys.argv[1], port=sys.argv[2])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run(host=sys.argv[1], port=sys.argv[2])
